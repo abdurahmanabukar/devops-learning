bb7c6b30994ee4dd1f4e30dfb1e8a80f511b3a38
abdurahmanabukar <abdurahmanabukar15@gmail.com>
2026-08-24 21:38:41 +0100

    Updated app.py

--- docker/challenge/app.py
+++ docker/challenge/app.py
@@ -1,1 +1,26 @@
+import os
+from flask import Flask
+import redis
 
+app = Flask(__name__)
+
+redis_host = os.environ.get("REDIS_HOST", "redis")
+redis_port = int(os.environ.get("REDIS_PORT", 6379))
+
+r = redis.Redis(
+    host=redis_host,
+    port=redis_port,
+    decode_responses=True
+)
+
+@app.route("/")
+def home():
+    return "Welcome to my Flask app!"
+
+@app.route("/count")
+def count():
+    visits = r.incr("visits")
+    return f"Visit count: {visits}"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
